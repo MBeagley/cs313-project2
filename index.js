7bdaad00f0aea9067c604a83996b3cec64020e55
MBeagley <bea11018@byui.edu>
2018-11-27 10:33:30 -0700

    testing

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ function authorize(credentials, callback) {
   });
 }
 
-*
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
+// *
+//  * Get and store new token after prompting for user authorization, and then
+//  * execute the given callback with the authorized OAuth2 client.
+//  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+//  * @param {getEventsCallback} callback The callback for the authorized client.
  
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
---
+++ myDB.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+  id SERIAL,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE notes (
+	id SERIAL,
+	name VARCHAR(100) NOT NULL,
+	userId INTEGER NOT NULL,
+	title VARCHAR(255),
+	content TEXT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+CREATE TABLE toDo (
+	id SERIAL,
+	userId INTEGER NOT NULL,
+	title VARCHAR(255),
+	done BOOLEAN,
+	PRIMARY KEY (id),
+	FOREIGN KEY (userId) REFERENCES users(id)
+);
